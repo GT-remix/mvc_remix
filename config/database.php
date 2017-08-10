7b8da2c120187fbb7d34a8b325047169c249b053
guo <25986505@qq.com>
2017-08-10 18:00:26 +0800

    init

---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+		'DB_HOST'   	=> 'localhost',
+		'DB_USER'   	=> 'root',
+		'DB_PASSWORD'   => '',
+		'DB_NAME'   	=> 'blog',
+		'DB_PREFIX'  	=> 'blog_',
+		'DB_CHARSET'   	=> 'utf8',
+		'DB_CACHE' 		=> './cache',
+	];
